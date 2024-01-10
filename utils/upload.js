5b5b3fa24def44919d8ce0b7677e6e263c925168
Aakash A Bhandari <102407592+Aakash-bhandari@users.noreply.github.com>
2024-01-10 08:25:20 +0530

    Update upload.js

--- utils/upload.js
+++ utils/upload.js
@@ -5,8 +5,9 @@ dotenv.config();
 
 const username=process.env.DB_username;
 const password=process.env.DB_password;
+const Db_url = process.env.DB_URL;
 const storage= new GridFsStorage({
-    url:`mongodb://${username}:${password}@ac-twjbyaa-shard-00-00.glqvfb8.mongodb.net:27017,ac-twjbyaa-shard-00-01.glqvfb8.mongodb.net:27017,ac-twjbyaa-shard-00-02.glqvfb8.mongodb.net:27017/?ssl=true&replicaSet=atlas-4mr2bg-shard-0&authSource=admin&retryWrites=true&w=majority`,
+    url:Db_url,
     options:{useNewUrlParser: true},
     file:(req,file)=>{
         const match=['image/png','image/jpg'];
